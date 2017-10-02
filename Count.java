bb4799f87e112e51cfce29874bd4cba6abd6121e
anicazulch <31353283+anicazulch@users.noreply.github.com>
2017-10-02 11:49:55 -0700

    Initial commit

---
+++ Count.java
@@ -0,0 +1,26 @@
+// ************************************************************************************
+// Name of Program: Count
+// Author: A. Zulch
+// Program description: This program counts from 1 to 5 in English, French, and Spanish
+// *************************************************************************************
+public class Count
+{
+ 
+    public static void main (String[] args)
+    {
+        
+        // This line will print in English
+        System.out.println ("one two three four five");
+        
+        // This line will print in French // Is this a problem?
+        System.out.println ("un deux trois quatre cinq");
+        
+        // This line will print in Spanish
+        System.out.println ("uno dos tres cuatro cinco");
+    }
+    
+    // Answers: 
+    // 2. The program is easier to read with the blank spaces
+    // 3. The error says that it is an illegal start to an expression
+    // 4. The comment within a comment does not cause problems
+}
---
+++ Simple.java
@@ -0,0 +1,24 @@
+ 
+// ********************************************
+//  Simple.java
+//
+//  Print a simple message about Java.
+//          
+// ********************************************
+ 
+public class Simple
+{
+ 
+    public static void main (String[] args)
+    {
+ 
+        System.out.println ("Java rocks!!");
+ 
+    }
+    // Answers:
+    // 1. 
+    // 2. 
+    // 3. 
+    // 4. 
+}
+
